c98d104816d9604f0be7b97d8b6db5d7d39cdcd8
Jörg Kantel <joerg@kantel.de>
2020-08-01 20:45:19 +0200

    Pluto getestet

---
+++ pluto/hello.jl
@@ -0,0 +1,23 @@
+### A Pluto.jl notebook ###
+# v0.11.2
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 6c340276-d41e-11ea-02b3-358efeaf544b
+println("Julia 👩‍🦰 sagt: Hallo, Jörg!")
+
+# ╔═╡ 02869d36-d41f-11ea-374c-e7070aed0782
+a = 10
+
+# ╔═╡ 0cfea038-d41f-11ea-0f40-db0fa69142eb
+b = 5
+
+# ╔═╡ 169080b0-d41f-11ea-18ee-6bee7fba1a92
+a + b
+
+# ╔═╡ Cell order:
+# ╠═6c340276-d41e-11ea-02b3-358efeaf544b
+# ╠═02869d36-d41f-11ea-374c-e7070aed0782
+# ╠═0cfea038-d41f-11ea-0f40-db0fa69142eb
+# ╠═169080b0-d41f-11ea-18ee-6bee7fba1a92
--- sources/plot.jl
+++ sources/plot.jl
@@ -1,4 +1,5 @@
 using Gadfly
 p = plot(y = [1, 2, 3, 4, 5])
-img = SVG("plot1.svg", 8cm, 8cm)
+img = SVG("plot3.svg", 8cm, 8cm)
 draw(img, p)
+println("Julia 👩‍🦰 sagt: I did it, Babe!")
